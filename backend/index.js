b33b73aa203f5b6396e218d11674543e287d8c82
Taha-M-Fawy <tahafawy2@gmail.com>
2026-09-18 17:17:15 +0300

    updata merge requests

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
 const cors = require('cors');
-=======
-const cors = require('cors'); 
->>>>>>> main
 const { connectDB } = require('./config/db.config');
 const { PORT } = require('./config/env.config');
 
@@ -13,33 +9,26 @@ const userRoutes = require('./routes/user.route');
 const categoryRoutes = require('./routes/category.route');
 const reviewsRoutes = require('./routes/reviews.routes');
 const notificationsRoutes = require('./routes/notifications.routes');
-<<<<<<< HEAD
 const requestsRoutes = require('./routes/requests.routes');
-
-=======
 const locationRoutes = require('./routes/location.routes');
->>>>>>> main
+
 // Middlewares Imports
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
-app.use(cors());
 
-<<<<<<< HEAD
-// Global Middleware
-=======
+// Settings & Global Middlewares
+app.set('etag', false);
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id']
+}));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({
-  origin: 'http://localhost:4200',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id']
-}));
-
->>>>>>> main
-app.use(express.json());
 
 // API Routes
 app.use('/api/items', itemRoutes);
@@ -47,20 +36,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/reviews', reviewsRoutes);
 app.use('/api/notifications', notificationsRoutes);
-<<<<<<< HEAD
 app.use('/api/requests', requestsRoutes);
+app.use('/api/locations', locationRoutes);
 
-// Error Handling
+// Error Handling Middleware (Always after routes)
 app.use(errorHandler);
 
 // Connect DB & Start Server
-=======
-app.use('/api/locations', locationRoutes);
-//*--- ERROR HANDLING MIDDLEWARE ---*//
-app.use(errorHandler);
-app.set('etag', false);
-//*--- CONNECT DB & START SERVER ---*//
->>>>>>> main
 const serverPort = PORT || 5000;
 
 connectDB()
